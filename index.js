3fb2587d47896479ae5d706d0aa96e7e995f6649
An KangMin <hkkm612@gmail.com>
2020-06-25 21:09:35 +0900

    Express Core - Middleware Part.2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan"; // 모든 연결에 대한 로그를 남겨주는 middleware
+import helmet from "helmet"; // NodeJS의 보안을 높여주는 middleware
+import cookieParser from "cookie-parser"; // 쿠키 분석을 위한 middlewaare
+import bodyParser from "body-parser"; // 요청의 본문을 분석하기 위한 middleware
+
 const app = express();
 
 const PORT = 4000;
@@ -15,15 +20,13 @@ const handleProfile = (req, res) => {
   res.send("You're in my profile");
 };
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
-
-app.use(betweenHome);
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
