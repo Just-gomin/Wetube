4415511c2f6e371232fa4532a329da7b2b97b51f
An KangMin <hkkm612@gmail.com>
2020-06-25 19:53:27 +0900

    First express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
