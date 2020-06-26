66b7f20b809bda9862fa0a9761696dabc095cce0
An KangMin <hkkm612@gmail.com>
2020-06-27 07:52:53 +0900

    MVC Pattern Part3 - Controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,11 @@
+export const join = (req, res) => {
+  return res.send("JOIN");
+};
+
+export const login = (req, res) => {
+  return res.send("LOGIN");
+};
+
+export const logout = (req, res) => {
+  return res.send("LOGOUT");
+};
---
+++ controllers/videoControllers.js
@@ -0,0 +1,7 @@
+export const home = (req, res) => {
+  return res.send("HOME");
+};
+
+export const search = (req, res) => {
+  return res.send("SEARCH");
+};
