933550babc0416b409202b42a25240783ad23045
An KangMin <hkkm612@gmail.com>
2020-06-28 14:16:18 +0900

    Local Variables in Pug

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import morgan from "morgan"; // 모든 연결에 대한 로그를 남겨주는 m
 import helmet from "helmet"; // NodeJS의 보안을 높여주는 middleware
 import cookieParser from "cookie-parser"; // 쿠키 분석을 위한 middlewaare
 import bodyParser from "body-parser"; // 요청의 본문을 분석하기 위한 middleware
+import { localsMiddleware } from "./middlewares";
 
 // Routers
 import globalRouter from "./routers/globalRouter";
@@ -16,15 +17,19 @@ import routes from "./routes";
 
 const app = express();
 
+// app의 보안을 향상 시켜줄 middleware
+app.use(helmet());
+
 // express app setting
 app.set("view engine", "pug"); // View Engine을 pug로 설정
 
 // middlewares 사용
-app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 // routers 사용
 app.use(routes.home, globalRouter);
