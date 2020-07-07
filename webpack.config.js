a8b83f4dcafcb1d3b4c31485b06fb58df95d2036
An KangMin <hkkm612@gmail.com>
2020-07-07 14:11:33 +0900

    Styles with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        // ExtractCSS는 CSS에서 텍스트만을 추출한다.
+        use: ExtractCSS.extract([
+          {
+            // CSS를 가져온다.
+            loader: "css-loader",
+          },
+          {
+            // postcss-loader는 특정 플러그인들을 css에 대해 실행시켜준다.
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              },
+            },
+          },
+          {
+            // sass-loader : Sass를 CSS로 바꿔준다.
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
+  },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
-  mode: "development",
+  plugins: [new ExtractCSS("styles.css")],
 };
 
 module.exports = config;
