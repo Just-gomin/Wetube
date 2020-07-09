f5ce849512242bd9f4443990d936f5713ad7c317
An KangMin <hkkm612@gmail.com>
2020-07-09 17:00:08 +0900

    GitHub Login Part1

--- passport.js
+++ passport.js
@@ -1,7 +1,22 @@
 import passport from "passport";
+import GitHubStrategy from "passport-github";
+import dotenv from "dotenv";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userControllers";
+dotenv.config();
 
 passport.use(User.createStrategy());
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/github/callback",
+    },
+    githubLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
