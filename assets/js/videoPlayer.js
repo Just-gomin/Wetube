217608bbd8e669755b7fc02bfdbbee3dd62d7a3c
An KangMin <hkkm612@gmail.com>
2020-07-16 14:28:40 +0900

    Fullscreen Functionallity.

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,47 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
-const video = videoContainer.querySelector("video");
+const video = document.querySelector("video");
 const playButton = document.getElementById("jsPlayButton");
+const volumeButton = document.getElementById("jsVolumeButton");
+const fullScreenButton = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (video.paused) {
     video.play();
+    playButton.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     video.pause();
+    playButton.innerHTML = '<i class="fas fa-play"></i>';
   }
+}
+
+function handleVolumeClick() {
+  if (video.muted) {
+    video.muted = false;
+    volumeButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    video.muted = true;
+    volumeButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
+function goSmallScreen() {
+  document.exitFullscreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-expand "></i>';
+  fullScreenButton.removeEventListener("click", goSmallScreen);
+  fullScreenButton.addEventListener("click", goFullScreen);
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenButton.removeEventListener("click", goFullScreen);
+  fullScreenButton.addEventListener("click", goSmallScreen);
 }
 
 function init() {
   playButton.addEventListener("click", handlePlayClick);
+  volumeButton.addEventListener("click", handleVolumeClick);
+  fullScreenButton.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
