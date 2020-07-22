eeeadcb02b8d2753bf2e390c42e6eedc7171a4fd
An KangMin <hkkm612@gmail.com>
2020-07-22 14:23:05 +0900

    Multer Uploads to AWS S3.

--- middlewares.js
+++ middlewares.js
@@ -3,12 +3,33 @@
 */
 
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
 
-// 비디오 파일을 videos라는 경로에 저장합니다.
-const multerVideo = multer({ dest: "uploads/videos/" });
-// 프로필 이미지를 avatars 경로상에 저장합니다.
-const multerAvatar = multer({ dest: "uploads/avatars/" });
+// S3 유저와 관련된 것들의 초기화.
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
+// 비디오 파일을 AWS의 S3 bucket에 저장합니다.
+const multerVideo = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "wetubebydorakang612/videos",
+  }),
+});
+// 프로필 이미지를 AWS의 S3 bucket에 저장합니다.
+const multerAvatar = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: "public-read",
+    bucket: "wetubebydorakang612/avatars",
+  }),
+});
 
 // 템플릿에서 변수들을 이용하기 위한 middleware입니다.
 export const localsMiddleware = (req, res, next) => {
